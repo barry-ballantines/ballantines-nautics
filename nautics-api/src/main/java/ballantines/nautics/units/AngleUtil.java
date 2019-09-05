ab45136185ad219290a3cdf873181399de3c7ecd
Barry Ballantines <barry.ballantines@mail.de>
2019-09-06 01:56:53 +0200

    nautics-api: AngleUtil added.

---
+++ nautics-api/src/main/java/ballantines/nautics/units/AngleUtil.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ballantines.nautics.units;
+
+import javax.measure.Quantity;
+import javax.measure.Unit;
+import javax.measure.quantity.Angle;
+import tec.units.ri.quantity.Quantities;
+
+/**
+ *
+ * @author mbuse
+ */
+public class AngleUtil {
+  
+  public static final Quantity<Angle> DEGREE_180 = Quantities.getQuantity(180., NauticalUnits.ARC_DEGREE);
+  public static final Quantity<Angle> DEGREE_360 = Quantities.getQuantity(360., NauticalUnits.ARC_DEGREE);
+  public static final Quantity<Angle> DEGREE_000 = Quantities.getQuantity(0., NauticalUnits.ARC_DEGREE);
+  
+  /**
+   * Normalizes a degree value to a range of ]-360., 360[
+   * @param angleInDegree
+   * @return an equivalent angle in range of ]-360, 360[ 
+   */
+  public static double normalizeDegrees(double angleInDegree) {
+    return angleInDegree % 360.;
+  }
+  
+  /**
+   * Normalizes a degree value to an equivalent degree of range [lowerBound, lowerBound+360°[.
+   * 
+   * @param degree
+   * @param lowerBound
+   * @return an angle of range [lowerBound, lowerBound+360°[.
+   */
+  public static double normalizeToLowerBound(double degree, double lowerBound) {
+    degree = normalizeDegrees(degree);
+    if (degree < lowerBound) {
+      return degree + 360.;
+    }
+    else if (degree >= (lowerBound + 360.)) {
+      return degree - 360.;
+    }
+    return degree;
+  }
+  
+  /**
+   * Normalizes a degree value to an equivalent degree of range ]upperBound-360°, upperBound].
+   * 
+   * @param degree
+   * @param upperBound
+   * @return an angle of range ]upperValue-360°, upperValue].
+   */
+  public static double normalizeToUpperBound(double degree, double upperBound) {
+    degree = normalizeDegrees(degree);
+    if (degree <= upperBound - 360.) {
+      return degree + 360.;
+    }
+    else if (degree > upperBound) {
+      return degree - 360.;
+    }
+    return degree;
+  }
+  
+  /**
+   * Compares two angles (given in degree)
+   * 
+   * @param phi   an angle in degree (°)
+   * @param theta an angle in degree (°)
+   * @return delta=theta-phi, negative values indicate left turn, positive values indicate right turn.
+   */
+  public static double delta(double phi, double theta) {
+    return normalizeToUpperBound(theta-phi, 180.);
+  }
+  
+  /**
+   * Normalizes a degree value to an equivalent degree of range [lowerBound, lowerBound+360°[.
+   * 
+   * @param degree
+   * @param lowerBound
+   * @return an angle of range [lowerBound, lowerBound+360°[.
+   */
+  public static Quantity<Angle> normalizeToLowerBound(Quantity<Angle> angle, Quantity<Angle> lowerBound) {
+    double angleDegrees = angle.to(NauticalUnits.ARC_DEGREE).getValue().doubleValue();
+    double lowerBoundDegrees = lowerBound.to(NauticalUnits.ARC_DEGREE).getValue().doubleValue();
+    double resultDegrees = normalizeToLowerBound(angleDegrees, lowerBoundDegrees);
+    if (resultDegrees == angleDegrees) { 
+      return angle; // UNCHANGED
+    }
+    else {
+      return Quantities.getQuantity(resultDegrees, NauticalUnits.ARC_DEGREE).to(angle.getUnit());
+    }
+  }
+  
+  /**
+   * Normalizes a degree value to an equivalent degree of range ]upperBound-360°, upperBound].
+   * 
+   * @param degree
+   * @param upperBound
+   * @return an angle of range ]upperValue-360°, upperValue].
+   */
+  public static Quantity<Angle> normalizeToUpperBound(Quantity<Angle> angle, Quantity<Angle> upperBound) {
+    double angleDegrees = angle.to(NauticalUnits.ARC_DEGREE).getValue().doubleValue();
+    double lowerBoundDegrees = upperBound.to(NauticalUnits.ARC_DEGREE).getValue().doubleValue();
+    double resultDegrees = normalizeToUpperBound(angleDegrees, lowerBoundDegrees);
+    if (resultDegrees == angleDegrees) { 
+      return angle; // UNCHANGED
+    }
+    else {
+      return Quantities.getQuantity(resultDegrees, NauticalUnits.ARC_DEGREE).to(angle.getUnit());
+    }
+  }
+  
+  /**
+   * Compares two angles (given in degree)
+   * 
+   * @param phi   the reference angle
+   * @param theta the test ange
+   * @return the angle from theta to phi (=theta-phi). normalized to ]-180°, 180°]. 
+   *         negative values indicate left turn, positive values indicate right turn.
+   */
+  public static Quantity<Angle>delta(Quantity<Angle> phi, Quantity<Angle> theta) {
+    return normalizeToUpperBound(theta.subtract(phi), DEGREE_180);
+  }
+}
---
+++ nautics-api/src/test/java/ballantines/nautics/units/AngleUtilTest.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ballantines.nautics.units;
+
+import javax.measure.Quantity;
+import javax.measure.quantity.Angle;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import tec.units.ri.quantity.Quantities;
+
+/**
+ *
+ * @author mbuse
+ */
+public class AngleUtilTest {
+  
+  public AngleUtilTest() {
+  }
+
+  /**
+   * Test of normalize method, of class AngleUtil.
+   */
+  @Test
+  public void testNormalizeDegrees() {
+    System.out.println("normalizeDegrees");
+    
+    assertEquals(123.2, AngleUtil.normalizeDegrees(123.2), 0.0);
+    assertEquals(-123.2, AngleUtil.normalizeDegrees(-123.2), 0.0);
+    
+    assertEquals(12.3, AngleUtil.normalizeDegrees(372.3), 0.000001);
+    assertEquals(-12.3, AngleUtil.normalizeDegrees(-372.3), 0.000001);
+    
+    
+  }
+
+  /**
+   * Test of normalizeToLowerBound method, of class AngleUtil.
+   */
+  @Test
+  public void testNormalizeToLowerBound() {
+    System.out.println("normalizeToLowerBound");
+    assertNormalizeToLowerBound( 180.,  180., AngleUtil.DEGREE_000);
+    assertNormalizeToLowerBound( 270.,  -90., AngleUtil.DEGREE_000);
+    assertNormalizeToLowerBound(   0.,  360., AngleUtil.DEGREE_000);
+    assertNormalizeToLowerBound(  90.,  450., AngleUtil.DEGREE_000);
+    assertNormalizeToLowerBound(  90.,  90. + 2 * 360., AngleUtil.DEGREE_000);
+  }
+  
+  private void assertNormalizeToLowerBound(double expected, double degrees, Quantity<Angle> lowerBound) {
+    Quantity<Angle> angle = Quantities.getQuantity(degrees, NauticalUnits.ARC_DEGREE);
+    Quantity<Angle> result = AngleUtil.normalizeToLowerBound(angle, lowerBound);
+    assertEquals(expected, result.getValue().doubleValue(), 0.000001);
+  }
+  
+  @Test
+  public void testNormalizeToUpperBound() {
+    System.out.println("normalizeToUpperBound");
+    assertNormalizeToUpperBound( 180. ,  180. , AngleUtil.DEGREE_180);
+    assertNormalizeToUpperBound(-179.9,  180.1, AngleUtil.DEGREE_180);  
+    assertNormalizeToUpperBound( 180. , -180. , AngleUtil.DEGREE_180);
+    assertNormalizeToUpperBound( -90. ,  -90. , AngleUtil.DEGREE_180);
+    assertNormalizeToUpperBound(   0. ,  360. , AngleUtil.DEGREE_180);
+    assertNormalizeToUpperBound(  90. ,  450. , AngleUtil.DEGREE_180); 
+  }
+  
+  private void assertNormalizeToUpperBound(double expected, double degrees, Quantity<Angle> upperBound) {
+    Quantity<Angle> angle = Quantities.getQuantity(degrees, NauticalUnits.ARC_DEGREE);
+    Quantity<Angle> result = AngleUtil.normalizeToUpperBound(angle, upperBound);
+    assertEquals(expected, result.getValue().doubleValue(), 0.000001);
+  }
+  
+}
