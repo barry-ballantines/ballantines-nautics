9ce6a584547c54468d19c3c78bb157291359c695
Barry Ballantines <barry.ballantines@mail.de>
2019-09-30 21:58:02 +0200

    nautics-grib: adding utility for tests

---
+++ nautics-grib/src/test/java/ballantines/nautics/grib/test/Grib2DecoderMain.java
@@ -0,0 +1,24 @@
+package ballantines.nautics.grib.test;
+
+import ballantines.nautics.grib.test.utils.ResourcesFileExportUtil;
+import com.noaa.grib.Grib2Decoder;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Grib2DecoderMain {
+
+  public static void main(String... args) throws Throwable {
+    File tmpFile = ResourcesFileExportUtil.exportResourceToTmpFile("/grib2/example.grb2");
+    System.out.println("Downloading test GRIB file to " + tmpFile);
+
+    Grib2Decoder.main(tmpFile.getPath());
+
+    if (tmpFile.delete()) {
+      System.out.println("GRIB File " + tmpFile + " deleted.");
+    }
+    else {
+      System.out.println("GRIB File " + tmpFile + " not deleted.");
+    }
+  }
+}
---
+++ nautics-grib/src/test/java/ballantines/nautics/grib/test/utils/ResourcesFileExportUtil.java
@@ -0,0 +1,39 @@
+package ballantines.nautics.grib.test.utils;
+
+import com.sun.jndi.toolkit.url.Uri;
+
+import java.io.*;
+import java.net.URI;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Paths;
+
+public class ResourcesFileExportUtil {
+
+  private static int DEFAULT_BUFFER_SIZE = 8192;
+
+  public static File exportResourceToTmpFile(String resourcePath) throws IOException {
+    URI uri = URI.create(resourcePath);
+    String filename = Paths.get(uri.getPath()).getFileName().toString();
+    String tmp = System.getProperty("java.io.tmpdir");
+    File outputFile = new File(tmp, filename);
+    exportResourceToFile(resourcePath, outputFile);
+    return outputFile;
+  }
+
+  public static void exportResourceToFile(String resourcePath, File outputFile) throws IOException {
+    InputStream is = ResourcesFileExportUtil.class.getResourceAsStream(resourcePath);
+    BufferedInputStream bis = new BufferedInputStream(is);
+
+    FileOutputStream fos = new FileOutputStream(outputFile);
+    try {
+      ReadableByteChannel rbc = Channels.newChannel(is);
+      fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+    }
+    finally {
+      fos.flush();
+      fos.close();
+    }
+
+  }
+}
