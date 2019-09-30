98cbcb5d638753ab2c50336d6b1666a6c0a9a67e
Barry Ballantines <barry.ballantines@mail.de>
2019-09-30 21:14:19 +0200

    nautics-grib: adding test classes
    Cloned from https://github.com/humbertodias/noaa-parse-grib-files.git

---
+++ nautics-grib/src/test/java/com/noaa/grib/Grib2Decoder.java
@@ -0,0 +1,26 @@
+package com.noaa.grib;
+
+import java.io.File;
+
+/**
+ *
+ * Home http://www.nco.ncep.noaa.gov/pmb/products/wave/ <code> 
+ * java GridDecoder http://www.ftp.ncep.noaa.gov/data/nccf/com/wave/prod/glwu.20180214/glwu.grlc_2p5km_sr.t02z.grib2
+ * </code>
+ *
+ * @author humbertodias
+ */
+public class Grib2Decoder {
+
+	public static void main(String ... args) throws Throwable {
+
+		if (args.length < 1) {
+			throw new RuntimeException("Usage GRIB2_URL_OR_FILE");
+		}
+
+		File gribFile = Util.downloadFileIfNecessary(args[0]);
+		Grib2Dumper grib2Dumper = new Grib2Dumper(gribFile);
+		grib2Dumper.dump(System.out, false);
+	}
+
+}
---
+++ nautics-grib/src/test/java/com/noaa/grib/Util.java
@@ -0,0 +1,103 @@
+package com.noaa.grib;
+
+//import org.apache.log4j.Logger;
+import ucar.grib.NoValidGribException;
+import ucar.grib.grib1.Grib1Input;
+import ucar.grib.grib2.Grib2Input;
+import ucar.unidata.io.RandomAccessFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Paths;
+
+public class Util {
+
+    //private static final Logger logger = Logger.getLogger(Util.class);
+
+    public static boolean isLocalFile(String path) {
+        return new File(path).exists();
+    }
+
+    public static File downloadFileIfNecessary(String filePath) throws Throwable {
+        if (isLocalFile(filePath))
+            return new File(filePath);
+        else
+            return downloadFileFromURL(filePath);
+    }
+
+    public static File downloadFileFromURL(String uri) throws Throwable {
+        return downloadFileFromURL(URI.create(uri));
+    }
+
+    public static File downloadFileFromURL(URI uri) throws Throwable {
+        String filename = Paths.get(uri.getPath()).getFileName().toString();
+        String tmp = System.getProperty("java.io.tmpdir");
+        File destination = new File(tmp, filename);
+        downloadFileFromURL(uri.toURL(), destination);
+        return destination;
+    }
+
+    public static void downloadFileFromURL(URL url, File destination) throws Throwable {
+        //logger.info("Downloading " + url.toExternalForm() + " in " + destination.getAbsolutePath());
+        try (
+                ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+                FileOutputStream fos = new FileOutputStream(destination)) {
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        }
+
+    }
+
+    public static ucar.unidata.io.RandomAccessFile toRandomAccessFile(File file) throws IOException {
+        return toRandomAccessFile(file, "r");
+    }
+
+    public static ucar.unidata.io.RandomAccessFile toRandomAccessFile(File file, String mode) throws IOException {
+        return new ucar.unidata.io.RandomAccessFile(file.getAbsolutePath(), mode);
+    }
+
+
+    public static void showGrib1(File gridFile) throws NoValidGribException, IOException {
+
+        RandomAccessFile randomFile = Util.toRandomAccessFile(gridFile);
+
+        //Create grib2Input file
+        Grib1Input grib2Input = new Grib1Input(randomFile);
+
+        grib2Input.scan(false, false);
+
+        grib2Input.getProducts().stream().forEach((r) -> {
+            System.out.println("Product.Header: " + r.getHeader());
+        });
+
+        System.out.println("Records.size:" + grib2Input.getRecords().size());
+
+        grib2Input.getRecords().stream().forEach((r) -> {
+            System.out.println("Record:" + r);
+        });
+
+    }
+
+    public static void showGrib2(File gridFile) throws IOException {
+
+        Grib2Dumper grib2Dumper = new Grib2Dumper(gridFile);
+        Grib2Input grib2Input = grib2Dumper.getInput();
+        grib2Input.getProducts().stream().forEach((p) -> {
+            grib2Dumper.printProduct(p, System.out);
+        });
+
+        grib2Input.getRecords().stream().forEach((r) -> {
+            try {
+                grib2Dumper.printRecord(r, System.out, true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
+
+}
