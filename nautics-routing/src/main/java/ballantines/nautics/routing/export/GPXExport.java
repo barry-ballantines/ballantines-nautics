9396dd067ba83d2d44b74fa770b7fa80c680ef88
Barry Ballantines <barry.ballantines@mail.de>
2019-10-04 22:10:10 +0200

    nautics-routing: GPXExport - logging additional information: boatSpeed, wind

--- nautics-routing/src/main/java/ballantines/nautics/routing/export/GPXExport.java
+++ nautics-routing/src/main/java/ballantines/nautics/routing/export/GPXExport.java
@@ -1,12 +1,15 @@
 package ballantines.nautics.routing.export;
 
 import ballantines.nautics.routing.Leg;
+import ballantines.nautics.units.AngleUtil;
 import ballantines.nautics.units.NauticalUnits;
 
 import java.io.PrintWriter;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+
+import static ballantines.nautics.units.NauticalUnits.*;
 
 public class GPXExport {
 
@@ -49,17 +52,28 @@ public class GPXExport {
     if (leg.parent!=null) {
       index = printLeg(out, leg.parent);
     }
-    Number lat = leg.endpoint.getLatitude().to(NauticalUnits.ARC_DEGREE).getValue();
-    Number lon = leg.endpoint.getLongitude().to(NauticalUnits.ARC_DEGREE).getValue();
+    Number lat = leg.endpoint.getLatitude().to(ARC_DEGREE).getValue();
+    Number lon = leg.endpoint.getLongitude().to(ARC_DEGREE).getValue();
     String timestamp = leg.time.toInstant().atZone(ZoneId.of("UTC")).format(DateTimeFormatter.ISO_INSTANT);
 
     out.println(String.format(Locale.US,"    <rtept lat=\"%.8f\" lon=\"%.8f\">", lat, lon));
     out.println("      <name>" + index +"</name>");
     out.println("      <time>" + timestamp + "</time>");
-    if (leg.bearing!=null && leg.distance!=null) {
+    if (leg.bearing!=null || leg.distance!=null || leg.boatSpeed!=null || leg.wind!=null) {
       out.println("      <cmt>");
-      out.println(String.format(Locale.US,"         Bearing  : %d°", leg.bearing.to(NauticalUnits.ARC_DEGREE).getValue().intValue()));
-      out.println(String.format(Locale.US,"         Leg dist : %d nm", leg.distance.to(NauticalUnits.NAUTICAL_MILE).getValue().intValue()));
+      if (leg.bearing!=null) {
+        out.println(String.format(Locale.US, "          Bearing : %d°", leg.bearing.to(ARC_DEGREE).getValue().intValue()));
+      }
+      if (leg.distance!=null) {
+        out.println(String.format(Locale.US, "         Leg dist : %.1f nm", leg.distance.to(NAUTICAL_MILE).getValue().floatValue()));
+      }
+      if (leg.boatSpeed!=null) {
+        out.println(String.format(Locale.US, "       Boat speed : %.1f kn", leg.boatSpeed.to(KNOT).getValue().floatValue()));
+      }
+      if (leg.wind!=null) {
+        out.println(String.format(Locale.US, "             Wind : %.1f kn, %d°", leg.wind.getRadial().to(KNOT).getValue().floatValue(),
+                Math.round(AngleUtil.normalizeToLowerBound(leg.wind.getAngle().to(ARC_DEGREE).getValue().doubleValue(), 0.))));
+      }
       out.println("      </cmt>");
     }
     out.println("    </rtept>");
